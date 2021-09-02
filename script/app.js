e0954fdc4d880cfc4731649d0b9e78ee44f186db
Bo Nicolajsen <squirrelworks@users.noreply.github.com>
2021-09-02 11:06:42 +0200

    test commit
    røv

--- script/app.js
+++ script/app.js
@@ -1,6 +1,4 @@
 
-// get the Dom element to work within
-let app=document.getElementById("app");
 
 // load your data
 let myFetchedData=loadData('https://dog.ceo/api/breeds/image/random');
@@ -8,20 +6,29 @@ let myFetchedData=loadData('https://dog.ceo/api/breeds/image/random');
 
 
 //Load Data function
-function loadData(myUrl){
+function loadData(apiUrl){
 
     console.log("fetching data");
    
 
-    fetch(myUrl)
+    fetch(apiUrl)
 
-    .then((response) => {
+    .then(   (response) => {
       // wait for fetch to complete
+
+
         return response.json();
-      })
+
+
+
+
+
+      }
+      )
       
-      .then((data) => {
-        // do something with 'data'
+      .then(    (data) => {
+        // do something with 'data' 
+
         buildView(data.message);
       
       })
@@ -31,11 +38,50 @@ function loadData(myUrl){
       );
 }
 
+
+
+
+
 // dom setup
-function buildView(myUrl){
+// get the Dom element to work within
+let app=document.getElementById("app");
+
+
+function buildView(myImgUrl){
+
+
+  let myTitle=document.createElement("h2");
+
+  myTitle.innerText="En Hund";
    
 let dogImg=document.createElement("img");
-dogImg.src=myUrl;
+dogImg.src=myImgUrl;
+
+app.appendChild(myTitle);
 app.appendChild(dogImg);
 
 }
+
+
+
+/*
+ <script> 
+          
+        // Get HTML head element 
+        var head = document.getElementsByTagName('HEAD')[0];  
+  
+        // Create new link Element 
+        var link = document.createElement('link'); 
+  
+        // set the attributes for link element  
+        link.rel = 'stylesheet';  
+      
+        link.type = 'text/css'; 
+      
+        link.href = 'style.css';  
+  
+        // Append link element to HTML head 
+        head.appendChild(link);  
+    </script>
+    */
+   
